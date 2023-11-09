5cb2f6216b30c9d28dc8b91c26ca01aae5384d69
huberyxxiao <huberyxxiao@tencent.com>
2023-11-09 19:38:19 +0800

    fix

--- src/op/object_op.cpp
+++ src/op/object_op.cpp
@@ -1724,9 +1724,7 @@ std::string ObjectOp::GeneratePresignedUrl(const GeneratePresignedUrlReq& req) {
     }
     query_str += part;
   }
-  if (query_str != "") {
-    signed_url += "&";
-  }
+
   signed_url += query_str;
   return signed_url;
 }
